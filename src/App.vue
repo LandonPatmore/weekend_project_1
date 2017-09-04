1bb5eec5d0ac6e40160d4389a829c0a136a245f4
Landon Patmore <landon.patmore@gmail.com>
2017-09-03 20:09:36 -0400

    Dummy data and search
    - added dummy data from real API so that we don't have to keep polling their API
    - added a search function to the driver stats
       - can now search by driver name, nationality, team, and points
    - changed view of tables, for ease of viewing for now

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,48 @@
   <div id="app">
     <h1>F1 Standings</h1>
     <h4>Select a year</h4>
-    <select v-model='selectedYear'>
-      <option v-for='year in setOfYears' :key='year' :value='year'>{{year}}</option>
+    <select v-model='selectedYear' disabled>
+      <option v-for='year in setOfYears' :key='year' :value='year'>{{year}} </option>
     </select>
-    <h2>Driver Standings</h2>
+    <input type="text" placeholder="Driver Information" v-model="driverSearch">
     <table>
       <thead>
         <th>Position</th>
         <th>Name</th>
+        <th>Nationality</th>
         <th>Points</th>
         <th>Wins</th>
         <th>Team</th>
       </thead>
       <tbody>
-        <tr v-for="driver in driversData" :key="driver.position">
+        <tr v-for="driver in filteredDrivers" :key="driver.position">
           <td>{{driver.position}}</td>
           <td>{{driver.Driver.familyName}}</td>
+          <td>{{driver.Driver.nationality}}</td>
           <td>{{driver.points}}</td>
           <td>{{driver.wins}}</td>
           <td>{{driver.Constructors[0].name}}</td>
         </tr>
       </tbody>
     </table>
-    <hr>
-    <h2>Constructor Standings</h2>
-    <router-view></router-view>
+    <table>
+      <thead>
+        <th>Position</th>
+        <th>Name</th>
+        <th>Points</th>
+        <th>Wins</th>
+        <th>Nationality</th>
+      </thead>
+      <tbody>
+        <tr v-for="team in teamData" :key="team.position">
+          <td>{{team.position}}</td>
+          <td>{{team.Constructor.name}}</td>
+          <td>{{team.points}}</td>
+          <td>{{team.wins}}</td>
+          <td>{{team.Constructor.nationality}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -41,8 +58,11 @@ export default {
       currentYear: new Date().getFullYear(),
 
       driversData: [],
+      teamData: [],
 
-      selectedYear: ''
+      selectedYear: '',
+
+      driverSearch: ''
     }
   },
 
@@ -60,63 +80,103 @@ export default {
       return setOfYears
     },
 
-    tester: function() {
-      console.log('tester method')
+    grabDriverStandings: function(year) {
+      return axios.get('../static/driverData.json')
     },
 
-    grabDriverStandings: function(year) {
-      axios.get(`http://ergast.com/api/f1/` + year + `/driverStandings.json`)
-        .then(response => {
-          // JSON responses are automatically parsed.
-          this.driversData = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings
-        })
-        .catch(e => {
-          this.errors.push(e)
-        })
+    grabConstructorStandings: function(year) {
+      return axios.get(`../static/teamData.json`)
+    },
+
+    grabAllData: function(year) {
+      var that = this
+
+      axios.all([this.grabDriverStandings(year), this.grabConstructorStandings(year)])
+        .then(axios.spread(function(drivers, constructors) {
+          that.driversData = drivers.data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+          that.teamData = constructors.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings
+        }))
     }
   },
 
   watch: {
     selectedYear: function() {
-      this.grabDriverStandings(this.selectedYear)
+      this.grabAllData(this.selectedYear)
     }
   },
 
   created: function() {
     this.setOfYears = this.setYears()
-    this.grabDriverStandings(this.currentYear)
+    this.grabAllData(this.currentYear)
+  },
+
+  computed: {
+    filteredDrivers: function() {
+      return this.driversData.filter((driver) => {
+        return driver.Driver.familyName.match(new RegExp(this.driverSearch, 'i')) || driver.Driver.nationality.match(new RegExp(this.driverSearch, 'i')) || driver.points.match(new RegExp(this.driverSearch, 'i')) || driver.Constructors[0].name.match(new RegExp(this.driverSearch, 'i'))
+      })
+    }
   }
 }
 </script>
 
 <style lang='scss'>
-#app {}
+@import url('https://fonts.googleapis.com/css?family=Roboto');
 
-select {
-  font-size: 1.5rem;
-}
+$font: Roboto,
+sans-serif;
+$transparency: rgba(255,
+255,
+255,
+0.3);
 
-table {
-  text-align: center;
-  border-collapse: collapse;
+#app {
+  font-family: $font;
+  height: 100%;
+  width: 100%;
 
-  thead {
-    font-size: 1.2rem;
+  h1 {
+    margin: 0;
+    font-size: 3.0rem;
   }
 
-  td,
-  th {
-    border: 1px solid black;
-    padding: 0.2rem;
-    min-width: 100px;
+  select {
+    font-size: 1.5rem;
   }
 
-  tr:hover{
-    background: red;
-  }
+  table {
+    text-align: left;
+    border-collapse: collapse;
+    table-layout: fixed;
+    text-transform: uppercase;
+    width: 45%;
+    margin: auto;
 
-  thead{
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    thead {
+      font-size: 1.2rem;
+    }
+
+    td,
+    th {
+      padding: 0.5rem;
+      min-width: 100px;
+    }
+
+    tr {
+      border: 1px solid $transparency;
+    }
+
+    tr:nth-child(odd) {
+      background: #EEE;
+    }
+
+    tr:hover {
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+
+    thead {
+      background: $transparency;
+    }
   }
 }
 </style>
